38c7c4dd30b4895b7d178d6f498921612d4ef7cf
Alec <ivnv.jr@gmail.com>
2020-08-16 10:19:18 -0400

    My first commit

---
+++ src/test/tests/HomePageTest.java
@@ -0,0 +1,35 @@
+package tests;
+
+import base.BaseTest;
+import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pages.HomePage;
+import pages.LogInPage;
+
+import java.lang.reflect.Method;
+
+public class HomePageTest extends BaseTest {
+    LogInPage logInPage;
+    HomePage homePage;
+
+    @BeforeMethod
+    public void localSetUp(Method method, ITestResult result){
+       setUp(method, result);
+        logInPage = new LogInPage(getDriver());
+
+    }
+    @Test
+    public void validatePageHeader(){
+        logInPage.logIn("standard");
+        Assert.assertEquals(homePage.headerText.getText(), "Products");
+    }
+    @Test
+    public void verifyLockedUser(){
+        logInPage.logIn("locked");
+        Assert.assertEquals(logInPage.errorMsg.getText(), "Products");
+
+    }
+
+}
---
+++ src/test/tests/LogInPageTest.java
@@ -0,0 +1,42 @@
+package tests;
+
+import base.BaseTest;
+import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pages.HomePage;
+import pages.LogInPage;
+
+import java.lang.reflect.Method;
+
+public class LogInPageTest extends BaseTest {
+    LogInPage logInPage;
+    HomePage homePage;
+
+    @BeforeMethod(alwaysRun = true)
+    public void setUp(Method method, ITestResult result){
+        super.setUp(method, result);
+        logInPage = new LogInPage(getDriver());
+        homePage = new HomePage(getDriver());
+    }
+
+    @Test
+    public void verifyTitle(){
+        logInPage.sleep(3000);
+        Assert.assertEquals(getDriver().getTitle(), "Swag Labs");
+
+
+    }
+    @Test
+    public void veryfyLogInStandardUser(){
+        logInPage.logIn("standard");
+        Assert.assertEquals(getDriver().getCurrentUrl(), "https://www.saucedemo.com/inventory.html");
+    }
+    @Test
+    public void verifyLockedUser(){
+        logInPage.logIn("locked");
+        Assert.assertEquals(logInPage.errorMsg.getText(), "Epic sadface: Sorry, this user has been locked out.");
+    }
+
+}
